0f0d4ad1178dd13df9f12431cf5f28864a1a989c
qdung030404 <qdung030404@gmail.com>
2025-11-06 21:53:56 +0700

    add recycleViews add recycleViews as a scrollview

--- app/src/main/java/com/example/appdonghua/Fragment/HomeFragment.java
+++ app/src/main/java/com/example/appdonghua/Fragment/HomeFragment.java
@@ -3,6 +3,8 @@ package com.example.appdonghua.Fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
@@ -11,10 +13,15 @@ import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridLayout;
 import android.widget.ImageButton;
 
 import com.example.appdonghua.Adapter.CarouselAdapter;
+import com.example.appdonghua.Adapter.CellAdapter;
+import com.example.appdonghua.Adapter.HotNovelScrollAdapter;
 import com.example.appdonghua.Model.Carousel;
+import com.example.appdonghua.Model.Cell;
+import com.example.appdonghua.Model.HotNovelScroll;
 import com.example.appdonghua.R;
 
 import java.util.ArrayList;
@@ -29,11 +36,14 @@ public class HomeFragment extends Fragment {
     private static final long AUTO_SCROLL_DELAY = 3000;
     private ViewPager2 carousel;
     private ImageButton search_Button;
-    private RecyclerView recyclerView;
+    private RecyclerView recyclerView, hotnovelScrollView;
     private List<Carousel> carouselItems;
     private CarouselAdapter carouselAdapter;
     private Handler autoScrollHandler;
     private Runnable autoScrollRunnable;
+    private CellAdapter cellAdapter;
+    private HotNovelScrollAdapter hotNovelScrollAdapter;
+
 
 
     // TODO: Rename parameter arguments, choose names that match
@@ -82,11 +92,16 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         initView(view);
         setupCarousel();
+        setupScroll();
+        initRecycleView();
+        initHotNovelScrollView();
         return view;
     }
     private void initView(View v){
         carousel = v.findViewById(R.id.carousel);
         search_Button = v.findViewById(R.id.search_Button);
+        recyclerView = v.findViewById(R.id.recyclerView);
+        hotnovelScrollView = v.findViewById(R.id.scollView);
     }
     private void setupCarousel(){
         carouselItems = new ArrayList<>();
@@ -98,14 +113,14 @@ public class HomeFragment extends Fragment {
         carouselAdapter = new CarouselAdapter(carouselItems);
         carousel.setAdapter(carouselAdapter);
     }
-    private void setupScoll(){
+    private void setupScroll(){
         autoScrollHandler = new Handler(Looper.getMainLooper());
         autoScrollRunnable = new Runnable() {
             @Override
             public void run() {
                 if (carousel != null && carouselAdapter != null){
                     int currentItem = carousel.getCurrentItem();
-                    int nextItem = currentItem + 1 % carouselItems.size();
+                    int nextItem = (currentItem + 1) % carouselItems.size();
                     carousel.setCurrentItem(nextItem, true);
                     autoScrollHandler.postDelayed(this, AUTO_SCROLL_DELAY);
 
@@ -113,6 +128,58 @@ public class HomeFragment extends Fragment {
             }
         };
         autoScrollHandler.postDelayed(autoScrollRunnable, AUTO_SCROLL_DELAY);
+
+        carousel.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+                if(state == ViewPager2.SCROLL_STATE_DRAGGING){
+                    stopAutoScroll();
+                }else if(state == ViewPager2.SCROLL_STATE_IDLE){
+                    startAutoScroll();
+                }
+            }
+        });
+    }
+    private void startAutoScroll() {
+        if(autoScrollHandler != null && autoScrollRunnable != null){
+            autoScrollHandler.removeCallbacks(autoScrollRunnable);
+            autoScrollHandler.postDelayed(autoScrollRunnable, AUTO_SCROLL_DELAY);
+        }
+    }
+    private void stopAutoScroll(){
+        if (autoScrollHandler != null && autoScrollRunnable != null){
+            autoScrollHandler.removeCallbacks(autoScrollRunnable);
+        }
+    }
+    private void initRecycleView(){
+        ArrayList<Cell> items =new ArrayList<>();
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+        items.add(new Cell(R.drawable.tghm, "Thế Giới Hoàn Mỹ"));
+
+        GridLayoutManager layoutManager =new GridLayoutManager(getContext(), 3);
+        recyclerView.setLayoutManager(layoutManager);
+        cellAdapter = new CellAdapter(items);
+        recyclerView.setAdapter(cellAdapter);
+
+    }
+    private void initHotNovelScrollView(){
+        ArrayList<HotNovelScroll> items =new ArrayList<>();
+        items.add(new HotNovelScroll(R.drawable.tghm, "Thế Giới Hoàn Mỹ", "30k", "Huyền Huyễn"));
+        items.add(new HotNovelScroll(R.drawable.tghm, "Thế Giới Hoàn Mỹ", "30k", "Huyền Huyễn"));
+        items.add(new HotNovelScroll(R.drawable.tghm, "Thế Giới Hoàn Mỹ", "30k", "Huyền Huyễn"));
+        items.add(new HotNovelScroll(R.drawable.tghm, "Thế Giới Hoàn Mỹ", "30k", "Huyền Huyễn"));
+        items.add(new HotNovelScroll(R.drawable.tghm, "Thế Giới Hoàn Mỹ", "30k", "Huyền Huyễn"));
+        LinearLayoutManager layoutManager =new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false);
+        hotnovelScrollView.setLayoutManager(layoutManager);
+        hotNovelScrollAdapter = new HotNovelScrollAdapter(items);
+        hotnovelScrollView.setAdapter(hotNovelScrollAdapter);
+
+
 
     }
 }
